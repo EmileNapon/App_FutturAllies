d0e168517993884b16d83d9001bf2eb80559e123
EmileNapon <emilenapon0@gmail.com>
2025-04-14 14:56:43 +0000

    first commit

--- Front-futurallies/src/app/admin/components/menu/menu.component.ts
+++ Front-futurallies/src/app/admin/components/menu/menu.component.ts
@@ -33,8 +33,8 @@ export class MenuComponent implements OnDestroy, AfterViewInit {
       children: [
         { label: 'Prog. Talent', route: 'formation' },
         { label: 'Webinaires', route: 'webinaire' },
-        { label: 'Cafe des Allies', route: 'trainings/coffee' },
-        { label: 'Formations externes', route: 'trainings/externes' }
+        { label: 'Cafe des Allies' },
+        { label: 'Formations externes' }
       ]
     },
 
@@ -43,17 +43,17 @@ export class MenuComponent implements OnDestroy, AfterViewInit {
       icon: 'menu_book',
       children: [
         { label: 'Domaines', route: 'domaine' },
-        { label: 'Parcours de formations', route: '/training-paths' },
-        { label: 'Cours libres', route: '/free-courses' },
-        { label: 'Cours payants', route: '/paid-courses' }
+        { label: 'Parcours de formations' },
+        { label: 'Cours libres'},
+        { label: 'Cours payants'}
       ]
     },
     {
       label: 'Offres',
       icon: 'work',
       children: [
-        { label: 'Emplois', route: 'list/offers/jobs' },
-        { label: 'Stages', route: '/internships' }
+        { label: 'Emplois' },
+        { label: 'Stages'}
       ]
     },
     {
@@ -69,52 +69,30 @@ export class MenuComponent implements OnDestroy, AfterViewInit {
       label: 'Certifications',
       icon: 'verified',
       children: [
-        { label: 'Organismes de certifications', route: '/certification-bodies' },
-        { label: 'Certifications disponibles', route: '/available-certifications' },
-        { label: 'Certifications délivrées', route: '/issued-certifications' }
+        { label: 'Organismes de certifications' },
+        { label: 'Certifications disponibles' },
+        { label: 'Certifications délivrées' }
       ]
     },
-    {
-      label: 'Rapports',
-      icon: 'assessment',
-      children: [
-        { label: 'Rapports de performance', route: '/performance-reports' },
-        { label: 'Rapports d’audit', route: '/audit-reports' }
-      ]
-    },
+
     {
       label: 'Interactions',
       icon: 'chat', // ou 'notifications', ou 'comment'
       children: [
-        { label: 'Notifications', route: 'interaction/notifications' },
-        { label: 'Messages', route: 'interaction/messages' },
-        { label: 'Mails', route: 'interaction/mails' }
+        { label: 'Notifications'},
+        { label: 'Messages' },
+        { label: 'Mails'}
       ]
     },
-    {
-      label: 'Support',
-      icon: 'help', // Icône d'aide ou de support
-      children: [
-        { label: 'FAQ', route: '/faq' },
-        { label: 'Tickets de support', route: '/support/tickets' },
-        { label: 'Aide en ligne', route: '/support/help' }
-      ]
-    },
-    {
-      label: 'Historique',
-      icon: 'history', // Icône d'historique
-      children: [
-        { label: 'Logs d’audit', route: '/audit-logs' },
-        { label: 'Modifications récentes', route: '/recent-changes' },
-      ]
-    },
+
+   
     {
       label: 'Paramètres',
       icon: 'settings',
       children: [
-        { label: 'Thèmes', route: '/themes' },
-        { label: 'Apparence', route: '/appearance' },
-        { label: 'Politique d’utilisation', route: '/policy' }
+        { label: 'Thèmes'},
+        { label: 'Apparence'},
+        { label: 'Politique d’utilisation' }
       ]
     },
 ];
