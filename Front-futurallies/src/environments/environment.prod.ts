fb8381731b22f58e6032490abf48335ad215d247
EmileNapon <emilenapon0@gmail.com>
2025-05-26 16:39:47 +0000

    first commit

--- Front-futurallies/src/environments/environment.prod.ts
+++ Front-futurallies/src/environments/environment.prod.ts
@@ -1,13 +1,13 @@
 
 
-// export const environment = {
-//   production: true,
-//   apiUrl: 'https://futurallies.com/fidalli'
-// };
-
-
-
 export const environment = {
-  production: false,
-  apiUrl: 'http://127.0.0.1:8000/fidalli'
+  production: true,
+  apiUrl: 'https://futurallies.com/fidalli'
 };
+
+
+
+// export const environment = {
+//   production: false,
+//   apiUrl: 'http://127.0.0.1:8000/fidalli'
+// };
